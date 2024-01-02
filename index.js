67ec62b98b4a74c0e9dffb5425b474086cbc56be
itsgichia <gichiamuiruri@gmail.com>
2024-01-02 19:16:22 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// Remove the <main> element
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+//Create a new <h1> element and set its id to 'victory'
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+// Set the inner text of the newHeader element
+newHeader.textContent = "Gichia is the champion";
+
+//Append the newHeader element to the document body or a specific container
+document.body.appendChild(newHeader);
